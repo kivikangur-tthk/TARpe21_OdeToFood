abb9c447433ae38c8e24104151c83bc83dcf8b6d
Kristjan Kivikangur <kristjan.kivikangur@tthk.ee>
2022-09-29 15:52:03 +0300

    Add UnitTest project and test HomeController About action

---
+++ OdeToFood.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OdeToFood.Controllers;
+using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Controllers.Tests
+{
+	[TestClass()]
+	public class HomeControllerTests
+	{
+		[TestMethod()]
+		public void AboutTest()
+		{
+			// Arrange
+			HomeController controller = new HomeController();
+			// Act
+			ViewResult result = controller.About() as ViewResult;
+			// Assert
+			Assert.IsNotNull(result.Model);
+			AboutModel aboutModel = result.Model as AboutModel;
+			Assert.AreEqual("Kristjan", aboutModel.Name);
+		}
+	}
+}
