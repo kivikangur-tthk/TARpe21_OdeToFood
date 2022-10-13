4948101dfda63596de6829f46cb76b618d508343
Kristjan Kivikangur <kristjan.kivikangur@tthk.ee>
2022-10-13 14:28:05 +0300

    Add search by name to homepage

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -19,18 +19,32 @@ namespace OdeToFood.Controllers
 			_db = dbContext;
 		}
 
-		public IActionResult Index()
+		public IActionResult Index(string searchTerm = null)
 		{
-			var model = from r in _db.Restaurants
-															orderby r.Reviews.Average(review => review.Rating)														
-															select new RestaurantListViewModel
-															{
-																Id = r.Id,
-																Name = r.Name,
-																City = r.City,
-																Country = r.Country,
-																CountOfReviews = r.Reviews.Count()
-															};
+			var model = _db.Restaurants
+				.OrderByDescending(r => r.Reviews.Average(review => review.Rating))
+				.Where(r => searchTerm == null || r.Name.StartsWith(searchTerm))
+				.Take(10)
+				.Select(r =>	new RestaurantListViewModel
+														{
+															Id = r.Id,
+															Name = r.Name,
+															City = r.City,
+															Country = r.Country,
+															CountOfReviews = r.Reviews.Count()
+														}
+				);
+
+			//var model = from r in _db.Restaurants
+			//												orderby r.Reviews.Average(review => review.Rating)
+			//												select new RestaurantListViewModel
+			//												{
+			//													Id = r.Id,
+			//													Name = r.Name,
+			//													City = r.City,
+			//													Country = r.Country,
+			//													CountOfReviews = r.Reviews.Count()
+			//												};
 
 			return View(model);
 		}
@@ -54,7 +68,7 @@ namespace OdeToFood.Controllers
 		}
 		protected override void Dispose(bool disposing)
 		{
-			if (_db!=null)
+			if (_db != null)
 			{
 				_db.Dispose();
 			}
