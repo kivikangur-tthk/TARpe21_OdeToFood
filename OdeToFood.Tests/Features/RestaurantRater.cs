113152e7969458dd9b731a9948de9a6dc7a15898
Kristjan Kivikangur <kristjan.kivikangur@tthk.ee>
2022-11-18 11:56:03 +0200

    TDD setup, do first cycle (RED->GREEN->REFACTOR)

---
+++ OdeToFood.Tests/Features/RestaurantRater.cs
@@ -0,0 +1,22 @@
+﻿using OdeToFood.Models;
+using System;
+
+namespace OdeToFood.Tests.Features
+{
+	public class RestaurantRater
+	{
+		private Restaurant _restaurant;
+
+		public RestaurantRater(Restaurant restaurant)
+		{
+			_restaurant = restaurant;
+		}
+
+		public RatingResult ComputeRating(int numberOfReviews)
+		{
+			var result = new RatingResult();
+			result.Rating = 4;
+			return result;
+		}
+	}
+}
---
+++ OdeToFood.Tests/Features/UnitTest11.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+
+namespace OdeToFood.Tests.Features
+{
+	[TestClass]
+	public class UnitTest11
+	{
+		[TestMethod]
+		public void MyTestMethod()
+		{
+			var data = new Restaurant();
+			data.Reviews = new List<RestaurantReview>();
+			data.Reviews.Add(new RestaurantReview() { Rating = 4 });
+
+			var rater = new RestaurantRater(data);
+			var result = rater.ComputeRating(10);
+
+			Assert.AreEqual(4, result.Rating);
+		}
+	}
+}
